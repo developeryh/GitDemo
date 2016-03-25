c6e7afd07ca4ffe92c044c3515bcf84fd5363b85
游辉 <developer_yh@163.com>
2016-03-25 14:09:00 +0800

    init

---
+++ GitDemo/ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by SR on 15/10/13.
+//  Copyright © 2015年 游辉. All rights reserved.
+//
+
+#import "ViewController.h"
+    
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    int aa = 10;
+    int bb = 11;
+    NSLog(@"%d%d",aa,bb);
+    [self test];
+    UIView *view = [[UIView alloc]init];
+    NSLog(@"%@",view);
+    NSLog(@"aa");
+    [self sayBayBay];
+    
+    
+    NSLog(@"xianzai");
+
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)sayBayBay{
+    NSLog(@"sayBayBay");
+}
+- (void)test{
+    NSLog(@"this is test");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
